f883bc0db0ed06cd38f8708b414f1a56e242cbd1
Saeed Rahmani <rahmanidashti@gmail.com>
2018-11-21 13:28:53 +0330

    Add files via upload

---
+++ FilmSite/Controllers/ContentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmSite.Controllers
+{
+    public class ContentController : Controller
+    {
+        public IActionResult Index()
+        {
+			var data = new
+			{
+				name = "Ali",
+				family = "Alizadeh"
+			};
+
+
+            return Json(data);
+        }
+
+		public IActionResult List()
+		{
+			return View();
+		}
+    }
+}
---
+++ FilmSite/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+// First Change!
+// third change!
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		public IActionResult About()
+		{
+			return View();
+		}
+    }
+}
